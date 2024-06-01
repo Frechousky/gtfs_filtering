6a2c84008018a755af1002cec220c8ec14a62be1
frechousky <alexandre.freche@gmail.com>
2024-06-01 22:06:26 +0200

    refactoring

--- gtfs_filtering/cli.py
+++ gtfs_filtering/cli.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-import typing
+from typing import List
 
-import click
+from click import command, option, argument, Choice, Path
 from gtfs_filtering.core import perform_filter, FilterType
 
 
-@click.command()
-@click.option('-o', '--overwrite', is_flag=True, default=False,
+@command()
+@option('-o', '--overwrite', is_flag=True, default=False,
               help='pass it to overwrite output GTFS zip if it exists')
-@click.option('-t', '--filter-type', type=click.Choice([f_type.value for f_type in FilterType]),
+@option('-t', '--filter-type', type=Choice([f_type.value for f_type in FilterType]),
               default=FilterType.ROUTE_ID.value, help='type of filtering')
-@click.argument('input_gtfs_zip', type=click.Path(exists=True, dir_okay=False))
-@click.argument('output_gtfs_zip', type=click.Path(dir_okay=False))
-@click.argument('filter_values', nargs=-1, required=True)
+@argument('input_gtfs_zip', type=Path(exists=True, dir_okay=False))
+@argument('output_gtfs_zip', type=Path(dir_okay=False))
+@argument('filter_values', nargs=-1, required=True)
 def cli(overwrite: bool, filter_type: str, input_gtfs_zip: str, output_gtfs_zip: str,
-        filter_values: typing.List[str]):
+        filter_values: List[str]):
     perform_filter(input_gtfs_zip, output_gtfs_zip, FilterType(filter_type), filter_values, overwrite)
 
 
