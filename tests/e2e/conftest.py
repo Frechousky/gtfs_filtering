e046bd9487bb7edfcedc2ae55afd1c076d603bbb
frechousky <alexandre.freche@gmail.com>
2024-06-06 22:36:13 +0200

    add some e2e tests

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,10 +1,35 @@
-import os
+from os.path import abspath, join, dirname
+from typing import List
+from zipfile import ZipFile
 
 import pytest
 
-ROOT_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+ROOT_FOLDER = abspath(join(dirname(__file__), '..', '..'))
 
 
 @pytest.fixture()
 def gtfs_nyc() -> str:
-    return os.path.join(ROOT_FOLDER, 'tests', 'data', 'gtfs_nyc.zip')
+    return join(ROOT_FOLDER, 'tests', 'data', 'gtfs_nyc.zip')
+
+
+@pytest.fixture()
+def existing_output_gtfs(tmp_path) -> str:
+    filepath = join(tmp_path, 'output_gtfs.zip')
+    with open(filepath, 'w') as f:
+        f.write('')     # empty zip file, just to test existence
+    return filepath
+
+
+@pytest.fixture()
+def output_gtfs(tmp_path) -> str:
+    return join(tmp_path, 'output_gtfs.zip')
+
+
+@pytest.fixture()
+def route_ids() -> List[str]:
+    return ['1', '2', '3']
+
+
+@pytest.fixture()
+def route_ids() -> List[str]:
+    return ['AFA23GEN-1038-Sunday-00_000600_1..S03R', 'AFA23GEN-1038-Sunday-00_009200_1..N03R']
--- tests/e2e/test_cli.py
+++ tests/e2e/test_cli.py
@@ -1,8 +1,9 @@
 import os.path
-import subprocess
-import typing
+from subprocess import run
+from typing import List
 
 import pytest
+
 from tests.e2e.conftest import ROOT_FOLDER
 
 CLI_PATH = os.path.join(ROOT_FOLDER, 'dist', 'cli')
@@ -11,28 +12,57 @@ CLI_PATH = os.path.join(ROOT_FOLDER, 'dist', 'cli')
 @pytest.mark.parametrize('args',
                          [['--help'], ['--help', 'gtfs.zip', 'output.zip'], ['gtfs.zip', '--help', 'output.zip'],
                           ['gtfs.zip', 'output.zip', '--help'], ['--help', 'gtfs.zip', 'output.zip', '--help'], ])
-def test_cli__when_help_option_is_present__displays_help(args: typing.List[str]):
-    output = subprocess.run([CLI_PATH, *args], capture_output=True, text=True)
+def test_cli__when_help_option_is_present__displays_help(args: List[str]):
+    output = run([CLI_PATH, *args], capture_output=True, text=True)
+
     assert output.returncode == 0, 'command should be successful'
     assert 'Usage: cli' in output.stdout, 'command should display help message to user'
 
 
 def test_cli__when_input_gtfs_zip_arg_is_missing__fails_and_displays_error_message():
-    output = subprocess.run(CLI_PATH, capture_output=True, text=True)
+    output = run(CLI_PATH, capture_output=True, text=True)
+
     assert output.returncode == 2, 'command should fail'
     assert 'Usage: cli' in output.stderr, 'command should display help message to user'
     assert "Error: Missing argument 'INPUT_GTFS_ZIP'." in output.stderr, 'command should display error message to user'
 
 
 def test_cli__when_input_gtfs_zip_does_not_exists__fails_and_displays_error_message():
-    output = subprocess.run([CLI_PATH, 'non_existing_file.zip'], capture_output=True, text=True)
+    output = run([CLI_PATH, 'non_existing_file.zip'], capture_output=True, text=True)
+
     assert output.returncode == 2, 'command should fail'
     assert 'Usage: cli' in output.stderr, 'command should display help message to user'
     assert "Invalid value for 'INPUT_GTFS_ZIP': File 'non_existing_file.zip' does not exist." in output.stderr, 'command should display error message to user'
 
 
-def test_cli__when_output_gtfs_zip_arg_is_missing__fails_and_displays_error_message(gtfs_nyc):
-    output = subprocess.run([CLI_PATH, gtfs_nyc], capture_output=True, text=True)
+def test_cli__when_output_gtfs_zip_arg_is_missing__fails_and_displays_error_message(gtfs_nyc: str):
+    output = run([CLI_PATH, gtfs_nyc], capture_output=True, text=True)
+
     assert output.returncode == 2, 'command should fail'
     assert 'Usage: cli' in output.stderr, 'command should display help message to user'
     assert "Error: Missing argument 'OUTPUT_GTFS_ZIP'." in output.stderr, 'command should display error message to user'
+
+
+def test_cli__when_output_gtfs_zip_exists_and_overwrite_flag_not_set__fails_and_displays_error_message(gtfs_nyc: str,
+                                                                                                       existing_output_gtfs: str,
+                                                                                                       route_ids: List[
+                                                                                                           str]):
+    assert os.path.isfile(existing_output_gtfs), 'output_gtfs exists'
+
+    output = run([CLI_PATH, gtfs_nyc, existing_output_gtfs, *route_ids], capture_output=True, text=True)
+
+    assert output.returncode == 1, 'command should fail'
+    assert f"Error: File '{existing_output_gtfs}' already exists." in output.stderr, 'command should display error message to user'
+
+
+@pytest.mark.parametrize('overwrite_opt', ['-o', '--overwrite'])
+def test_cli__when_output_gtfs_zip_exists_and_overwrite_flag_set__is_successful(gtfs_nyc: str, existing_output_gtfs: str,
+                                                                                route_ids: List[str],
+                                                                                overwrite_opt: str):
+    assert os.path.isfile(existing_output_gtfs), 'output_gtfs exists'
+    mtime_before = os.path.getmtime(existing_output_gtfs)
+
+    output = run([CLI_PATH, overwrite_opt, gtfs_nyc, existing_output_gtfs, *route_ids], capture_output=True, text=True)
+
+    assert output.returncode == 0, 'command is successful'
+    assert os.path.getmtime(existing_output_gtfs) > mtime_before, 'output gtfs should be overwritten'
